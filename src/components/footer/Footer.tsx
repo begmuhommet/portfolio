75770d4cd77ddebe2ddf22e5324c1f6838bdc2f0
Bega Jumayev <bega@ekta.io>
2023-09-18 11:26:19 +0800

    ✨ Added experience for tr language

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,13 +1,15 @@
+import i18n from '@/i18n';
+
 const Footer = () => {
   return (
     <footer className="relative py-4 bg-background-main text-white border-t border-secondary-light">
       <div className="container px-3 mx-auto">
         <div className="flex justify-center items-center mb-2">
-          <span className="mr-2">Designed</span>
+          <span className="mr-2">{i18n.t('designed')}</span>
           <span className="mr-2">&</span>
-          <span className="mr-2">Built</span>
-          <span className="mr-2">by</span>
-          <span className="mr-2 text-secondary-main">Jumayev Begmuhommet</span>
+          <span className="mr-2">{i18n.t('built')}</span>
+          {i18n.language === 'en' && <span className="mr-2">{i18n.t('by')}</span>}
+          <span className="mr-2 text-secondary-main">{i18n.t('name')}</span>
           <span>(2023)</span>
         </div>
       </div>
