4bc8a413daa07e2d399e684a9e24c876f79806e7
Bega Jumayev <bega@ekta.io>
2023-09-18 12:16:45 +0800

    🐛 Footer fix bugs

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,11 +4,13 @@ const Footer = () => {
   return (
     <footer className="relative py-4 bg-background-main text-white border-t border-secondary-light">
       <div className="container px-3 mx-auto">
-        <div className="flex justify-center items-center mb-2">
-          <span className="mr-2">{i18n.t('designed')}</span>
-          <span className="mr-2">&</span>
-          <span className="mr-2">{i18n.t('built')}</span>
-          {i18n.language === 'en' && <span className="mr-2">{i18n.t('by')}</span>}
+        <div className="flex flex-col sm:flex-row justify-center items-center mb-2">
+          <div>
+            <span className="mr-2">{i18n.t('designed')}</span>
+            <span className="mr-2">&</span>
+            <span className="mr-2">{i18n.t('built')}</span>
+            {i18n.language === 'en' && <span className="mr-2">{i18n.t('by')}</span>}
+          </div>
           <span className="mr-2 text-secondary-main">{i18n.t('name')}</span>
           <span>(2023)</span>
         </div>
